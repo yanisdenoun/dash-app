3aef725f709a4dd5532e96f39f8fa23a4330dc27
yanisdenoun <yanisdnn@gmail.com>
2023-12-10 13:14:29 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+# Import packages
+from dash import Dash, html, dash_table
+import pandas as pd
+
+
+# Incorporate data
+df_football_player_21_22 = pd.read_csv('data/2021-2022 Football Player Stats.csv', encoding='ISO-8859-1', delimiter=';') 
+df_football_player_22_23 = pd.read_csv('data/2022-2023 Football Player Stats.csv', encoding='ISO-8859-1', delimiter=';')
+df_football_team_21_22 = pd.read_csv('data/2021-2022 Football Team Stats.csv', encoding='ISO-8859-1', delimiter=';')
+df_football_team_22_23 = pd.read_csv('data/2022-2023 Football Team Stats.csv', encoding='ISO-8859-1', delimiter=';')
+
+
+
+# Initialize the app
+app = Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df_football_player_21_22.to_dict('records'), page_size=10)
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
