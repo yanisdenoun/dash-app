e6a391a974461ceb815348a88ac4386ba59f43db
yanisdenoun <yanisdnn@gmail.com>
2023-12-16 12:31:04 +0100

    dash theme

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Import packages
-from dash import Dash, html, dash_table
+from dash import Dash, dcc, html, Input, Output, dash_table, dash
+from sklearn.decomposition import PCA
+import plotly.express as px
 import pandas as pd
+import dash_bootstrap_components as dbc
 
 
 # Incorporate data
@@ -10,15 +13,17 @@ df_football_team_21_22 = pd.read_csv('data/2021-2022 Football Team Stats.csv', e
 df_football_team_22_23 = pd.read_csv('data/2022-2023 Football Team Stats.csv', encoding='ISO-8859-1', delimiter=';')
 
 
-
 # Initialize the app
 app = Dash(__name__)
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # App layout
 app.layout = html.Div([
     html.Div(children='My First App with Data'),
     dash_table.DataTable(data=df_football_player_21_22.to_dict('records'), page_size=10)
 ])
+
 
 # Run the app
 if __name__ == '__main__':
